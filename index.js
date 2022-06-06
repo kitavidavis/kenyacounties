899ebe90e2d5f1c044dd07ff3298216e29d92f5e
David Kitavi <geospatialeng2017@gmail.com>
2022-06-06 18:36:24 +0300

    removed importation bug

--- index.js
+++ index.js
@@ -7,60 +7,82 @@
 
 import counties from './counties';
 
-module.exports.getAll = function(){
-    return counties;
-}
+module.exports = {
 
-module.exports.getCountyByCode = function(code){
-    if(typeof(code) !== number){
-        throw new TypeError("County code must be a number!");
-    }
+    getAll: function(){
+        /**
+         * @returns []
+         */
 
-    if(code < 1 ){
-        throw new RangeError("Code should be in a range of 0 - 47");
-    }
+        return counties;
+    },
 
-    if(code > 47){
-        throw new RangeError("Code should be in a range of 0-47");
-    }
+    getCountyByCode: function(code){
+        /**
+         * @param code
+         * @returns {}
+         */
 
-    counties.map((item) => {
-        if(item.code === code){
-            return item;
+        if(typeof(code) !== number){
+            throw new TypeError("County code must be a number!");
         }
-    });
-
-    return null;
-}
-
-module.exports.getSubCountiesByCountyCode = function(code){
-    if(typeof(code) !== number){
-        throw new TypeError("County code must be a number!");
-    }
-
-    if(code < 1 ){
-        throw new RangeError("Code should be in a range of 0 - 47");
-    }
-
-    if(code > 47){
-        throw new RangeError("Code should be in a range of 0-47");
-    }
-
-    counties.map((item) => {
-        if(item.code === code){
-            return item.sub_counties;
+    
+        if(code < 1 ){
+            throw new RangeError("Code should be in a range of 0 - 47");
         }
-    });
-}
-
-module.exports.getSubCountiesByCountyName = function(name){
-    if(typeof(name) !== string){
-        throw new TypeError("County name should explicitely be a string");
-    }
-
-    counties.map((item) => {
-        if(item.name.toLowerCase() === name.toLowerCase()){
-            return item.sub_counties;
+    
+        if(code > 47){
+            throw new RangeError("Code should be in a range of 0-47");
         }
-    })
+    
+        counties.map((item) => {
+            if(item.code === code){
+                return item;
+            }
+        });
+    
+        return null;
+    },
+
+    getSubCountiesByCountyCode: function(code){
+        /**
+         * @param code
+         * @returns []
+         */
+
+        if(typeof(code) !== number){
+            throw new TypeError("County code must be a number!");
+        }
+    
+        if(code < 1 ){
+            throw new RangeError("Code should be in a range of 0 - 47");
+        }
+    
+        if(code > 47){
+            throw new RangeError("Code should be in a range of 0-47");
+        }
+    
+        counties.map((item) => {
+            if(item.code === code){
+                return item.sub_counties;
+            }
+        });
+    },
+
+    getSubCountiesByCountyName: function(name){
+        /**
+         * @param name
+         * @returns []
+         */
+        
+        if(typeof(name) !== string){
+            throw new TypeError("County name should explicitely be a string");
+        }
+    
+        counties.map((item) => {
+            if(item.name.toLowerCase() === name.toLowerCase()){
+                return item.sub_counties;
+            }
+        })
+    }
 }
